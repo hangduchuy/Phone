0a301c5980416a57b25bed9fb2e3882901b31f4b
vothanhtinh <vothanhtinh147@gmail.com>
2023-04-24 20:55:53 +0700

    update Danh Sach Don Hang

---
+++ WEBPHONE/Controllers/DonHangController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WEBPHONE.Models.BUSS;
+
+namespace WEBPHONE.Controllers
+{
+    public class DonHangController : Controller
+    {
+        // GET: DonHang
+        public ActionResult Index()
+        {
+            ViewBag.DonHang = DonHangBUS.LoadHoaDon(User.Identity.GetUserId());
+            return View();
+        }
+    }
+}
---
+++ WEBPHONE/Models/BUSS/DonHangBUS.cs
@@ -0,0 +1,18 @@
+﻿using ShopConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WEBPHONE.Models.BUSS
+{
+    public class DonHangBUS
+    {
+        public static List<HoaDon> LoadHoaDon(string mataikhoan)
+        {
+            var db = new ShopConnectionDB();
+            return db.Query<HoaDon>("select * from HoaDon where NguoiDat= @0", mataikhoan).ToList();
+
+        }
+    }
+}
